ca37a358ac45db4f9a0cbea90622847d9a985d4c
Berk Kaya <berk.kaya@tedu.edu.tr>
2024-08-07 23:28:15 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -14,11 +14,16 @@ const Home = () => {
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">I know so much technologies in computer Sciences.</p>
           </div>
-          <div>
-            <Button>
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button
+             variant="outline"
+             size="lg"
+             className="uppercase flex items-center gap-2"
+            >
               <span>Download CV</span>
               <FiDownload className="text-xl"/>
             </Button>
+            <div>Socials</div>
           </div>
           <div>photo</div>
         </div>
